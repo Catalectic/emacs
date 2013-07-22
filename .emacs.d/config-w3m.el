d572422071d1e57b274e3c864c4d1c346e3b3aa4
Catalectic <catalecticant@gmail.com>
2013-07-22 11:55:51 -0500

    w3m opens links in background tab and creates a new window

--- .emacs.d/config-w3m.el
+++ .emacs.d/config-w3m.el
@@ -1,4 +1,12 @@
- (setq browse-url-browser-function 'w3m-browse-url
-          browse-url-new-window-flag t)
+ (setq browse-url-browser-function 'w3m-browse-url-other-window
+          browse-url-new-window-flag t
+          w3m-new-session-in-background t)
  (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
  (global-set-key "\C-xm" 'browse-url-at-point)
+
+(defun w3m-browse-url-other-window (url &optional newwin)
+  (let ((w3m-pop-up-windows t))
+    (if (one-window-p) (split-window))
+    (other-window 1)
+    (w3m-browse-url url newwin)))
+ 
