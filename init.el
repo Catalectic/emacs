702446c40339d363ce1fedd63cb45c47596d7d29
Catalectic <catalecticant@gmail.com>
2013-08-08 14:26:06 -0500

    Automatically installs missing packages from ELPA

--- init.el
+++ init.el
@@ -8,6 +8,15 @@
         ("Tromey" . "http://tromey.com/elpa/")))
 (package-initialize)
 
+(setq my-packages '(smartparens dired+ ac-helm auto-complete bitlbee chm-view coffee-mode elscreen enh-ruby-mode flymake-coffee flymake-css flymake-ruby flymake-sass flymake-easy helm-descbinds helm-ls-git helm-projectile helm-rails helm-rubygems-local helm icomplete+ inflections popup projectile dash robe rspec-mode ruby-electric ruby-end ruby-tools rvm s sass-mode haml-mode ruby-mode session starter-kit-bindings starter-kit-eshell starter-kit-ruby starter-kit magit ido-ubiquitous smex find-file-in-project idle-highlight-mode paredit inf-ruby twittering-mode undo-tree w3m yari))
+
+(mapc
+ (lambda (package)
+   (or (package-installed-p package)
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package)) 
+           (package-install package))))
+ '(dired+ magit rainbow-mode))
+
 (require 'session)
 (session-initialize)
 (defun le::maybe-reveal ()
@@ -23,6 +32,7 @@
           'le::maybe-reveal)
 
 (require 'chm-view)
+(require 'web-mode)
 (require 'elscreen)
 (elscreen-start)
 
