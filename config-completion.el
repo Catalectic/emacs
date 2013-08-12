e4126ae13e29785a96cfb912bc6fae51cc6f9627
Catalectic <catalecticant@gmail.com>
2013-08-12 11:35:35 -0500

    Finally got yasnippet to work

--- config-completion.el
+++ config-completion.el
@@ -23,32 +23,14 @@
   (interactive)
   (dotimes (i 10) (ac-previous)))
 
+(require 'yasnippet)
+(require 'helm-c-yasnippet)
+(setq helm-c-yas-space-match-any-greedy t)
+(yas--initialize)
+(yas-reload-all)
+(yas-global-mode)
+
 ; Hack for performance in Helm, especially to switch buffers
 (defun helm-highlight-buffers (x y) nil)
-
-(require 'icomplete+)
-
-; Icicle
-
-;; (idle-highlight-mode 0)
-
-;; (setq icicle-buffers-ido-like-flag t
-;;       icicle-incremental-completion nil
-;;       icicle-expand-input-to-common-match 1
-;;       enable-recursive-minibuffers t)
-
-;; ;(global-set-key (kbd "<f1>") 'icicle-complete-keys)
-
-;;   (dolist (map (append (list minibuffer-local-completion-map
-;; 			   minibuffer-local-must-match-map)
-;; 		       (when (boundp 'minibuffer-local-filename-completion-map)
-;; 		         (list minibuffer-local-filename-completion-map))))
-;;     (define-key map [up] 'previous-history-element)
-;;     (define-key map [down] 'next-history-element)
-;;  (define-key map [?\M-p] 'icicle-previous-prefix-candidate)
-;;     (define-key map [?\M-n] 'icicle-next-prefix-candidate)
-;; (define-key map (kbd "<tab>") 'icicle-apropos-complete)
-;; (define-key map (kbd "C-o") 'icicle-prefix-complete)
-;; (define-key map (kbd "C-s") 'icicle-narrow-candidates))
 
 (provide 'config-completion)
