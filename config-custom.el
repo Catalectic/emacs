eb2f52fbbb7444dfaf7496b1413f39e2a4d4bf3b
Catalectic <catalecticant@gmail.com>
2014-01-30 17:25:50 -0800

    Add ack for projectile

--- config-custom.el
+++ config-custom.el
@@ -4,8 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-quick-help-delay 1.0)
+ '(ack-and-a-half-executable "/usr/local/bin/ack")
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
- '(auto-indent-key-for-end-of-line-then-newline "<M-RET>")
+ '(auto-indent-key-for-end-of-line-then-newline "<M-RET>" t)
  '(auto-indent-on-save-file nil)
  '(custom-enabled-themes (quote (solarized-dark)))
  '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
@@ -22,6 +23,7 @@
  '(js-indent-level 2)
  '(kill-whole-line t)
  '(menu-bar-mode t)
+ '(projectile-globally-ignored-directories (quote (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" "venv" "build" "log")))
  '(scroll-error-top-bottom t)
  '(session-use-package t nil (session))
  '(show-paren-mode t)
