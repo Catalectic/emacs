1c2a21b9062211be10e60b639bf41497ce4e0492
Catalectic <catalecticant@gmail.com>
2015-01-17 19:22:25 -0800

    Misc config changes

--- config-custom.el
+++ config-custom.el
@@ -5,40 +5,36 @@
  ;; If there is more than one, they won't work right.
  '(ac-quick-help-delay 1.0)
  '(ack-and-a-half-executable "/usr/local/bin/ack")
- '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(ansi-color-names-vector
+   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
  '(auto-indent-key-for-end-of-line-then-newline "<M-RET>" t)
  '(auto-indent-on-save-file nil)
  '(custom-enabled-themes (quote (solarized-dark)))
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
+ '(custom-safe-themes
+   (quote
+    ("6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "e16a771a13a202ee6e276d06098bc77f008b73bbac4d526f160faa2d76c1dd0e" "fc6e906a0e6ead5747ab2e7c5838166f7350b958d82e410257aeeb2820e8a07a" default)))
  '(delete-selection-mode t)
+ '(erc-server-reconnect-attempts t)
  '(font-use-system-font nil)
  '(global-auto-complete-mode t)
  '(global-hi-lock-mode nil)
  '(global-undo-tree-mode t)
  '(global-visual-line-mode t)
- '(helm-adaptative-mode t nil (helm-adaptative))
+ '(haskell-mode-hook (quote (turn-on-haskell-indent)))
  '(helm-ff-auto-update-initial-value nil)
- '(ido-ubiquitous-mode t)
- '(iswitchb-mode nil)
  '(js-indent-level 2)
  '(kill-whole-line t)
- '(menu-bar-mode t)
- '(projectile-globally-ignored-directories (quote (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" "venv" "build" "log")))
+ '(projectile-globally-ignored-directories
+   (quote
+    (".idea" ".eunit" ".git" ".hg" ".fslckout" ".bzr" "_darcs" "venv" "build" "log")))
  '(scroll-error-top-bottom t)
- '(session-use-package t nil (session))
- '(show-paren-mode t)
- '(smartparens-global-mode t)
  '(sp-highlight-pair-overlay nil)
  '(sp-highlight-wrap-overlay nil)
  '(sp-highlight-wrap-tag-overlay nil)
- '(transient-mark-mode t)
  '(warning-minimum-level :error))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:weight extra-bold))))
- '(sp-pair-overlay-face ((((class color) (min-colors 89)) (:background "#eee8d5"))))
- '(sp-wrap-overlay-face ((((class color) (min-colors 89)) (:background "#eee8d5"))))
- '(sp-wrap-tag-overlay-face ((((class color) (min-colors 89)) (:background "#eee8d5")))))
+ )
