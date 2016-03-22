8e841bf771f2026d00f329aeaa849e57e1379282
Daphne Johnson <daphne.johnson@placed.com>
2016-03-22 16:17:16 -0700

    Add Slack Emacs support

--- config-irc.el
+++ config-irc.el
@@ -9,4 +9,17 @@
       circe-default-user "Circe"
       circe-reduce-lurker-spam t)
 
+(require 'slack)
+(setq slack-buffer-emojify t)
+(setq slack-room-subscription nil)
+
+(defmethod slack-room-subscribedp ((_room slack-room) _team)
+  t)
+
+(defmethod slack-room-subscribedp ((room slack-group) _team)
+  t)
+
+(require 'alert)
+(setq alert-default-style 'notifications)
+
 (provide 'config-irc)
